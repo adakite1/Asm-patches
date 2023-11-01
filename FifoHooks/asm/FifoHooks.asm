f1c632bfc1d03692ceb7eb8de0f0db83e3e0e8bd
adakite1 <adakite.a@gmail.com>
2023-11-01 17:07:00 +0800

    Let armips automatically generate branches; added more documentation

--- FifoHooks/asm/FifoHooks.asm
+++ FifoHooks/asm/FifoHooks.asm
@@ -22,11 +22,18 @@
 ; This is included for reference. These are the APIs for pushing events over the IPC.
 ; After branching, they will return a negative value if an error occurred, or zero on success, so the usual way
 ; to ensure a message is sent is to loop until it returns zero.
-.definelabel PushToFifoArm7, 0x37FE448
-.definelabel PushFormattedMessageToFifoArm9, 0x0207db20
+.definelabel PushToFifoArm7, 0x37FE448                  ; r0 = 32-bit message, must be formatted as follows:
+; ====== MESSAGE FORMATTING ======
+; Fifo messages should be formatted as follows:
+; 00000000000000000000000000011111
+; --------------------------*=====
+; 26-bit message             5-bit event type (at most 32 event types can be registered in total)
+;                           single-bit flag
+.definelabel PushFormattedMessageToFifoArm9, 0x207DB20  ; r0 = event channel number (set to 0x12 for FifoHooks messages), r1 = 26-bit message, r2 = flag.
 
 .definelabel Arm9RegisterFifoCallbackHook, 0x207D284
 .definelabel RegisterIpcEventTypeCallback_Arm9, 0x207DAB0
+; Derived from the end address of the arm9 entry function zero-fill, also the load address for overlay0, which is loaded in after the callback set is complete.
 .definelabel Arm9TemporaryHook, 0x022BCA80 ; This hook will be overwritten by other code, but it will be used before that happens so it's ok.
 
 .open "arm9.bin", 0x02000000
@@ -61,15 +68,15 @@
 .gba
 .arm
 
-.definelabel Arm7RegisterFifoCallbackHook, 0x238A0D8 ; Actual address: 0x3801EF0
+.definelabel Arm7RegisterFifoCallbackHook, 0x3801EF0 ; Address before mapping:    0x238A0D8
 .definelabel RegisterIpcEventTypeCallback_Arm7, 0x37FE39C ; Address before mapping: 0x2386584
 .definelabel DSEEventsTable, 0x20B0950
 
-.open "arm7.bin", 0x02380000
+.open "arm7.bin", 0x037F7E18
 	.org Arm7RegisterFifoCallbackHook
 	.area 0x4
-        .word 0b11101010101000101011101110001111 ; Branch to Arm7RegisterCallback
-        ; Subsequent address: 0x3801EF4
+        b Arm7RegisterCallback
+arm7_register_callback_return:
 	.endarea
 .close
 .open "arm9.bin", 0x02000000
@@ -95,7 +102,7 @@
         ; Original instruction
         mov r0,#7h
 
-        .word 0b11101010010111010100010001101000 ; Actual address: 0x20B0D4C, need to branch back to 0x3801EF4
+        b arm7_register_callback_return
 	.endarea
 .close
 
