4912552f4a5d0f8c30c3490e929f0e5885eacf7b
adakite1 <adakite.a@gmail.com>
2023-11-01 17:41:57 +0800

    Added EU region support for FifoHooks

--- FifoHooks/asm/FifoHooks.asm
+++ FifoHooks/asm/FifoHooks.asm
@@ -14,27 +14,14 @@
 ;  - 0x0             callback address
 ;  - 0x4 onwards     any data.
 ;  An additional argument r3 will contain either 7 or 9 depending on which processor the callback was called from.
+; 
+; See the offsets file for each region for information on the functions used to send messages.
+
+.include "regionSelect.asm"
 
 .nds
 
 .definelabel EventType, 0x12
-
-; This is included for reference. These are the APIs for pushing events over the IPC.
-; After branching, they will return a negative value if an error occurred, or zero on success, so the usual way
-; to ensure a message is sent is to loop until it returns zero.
-.definelabel PushToFifoArm7, 0x37FE448                  ; r0 = 32-bit message, must be formatted as follows:
-; ====== MESSAGE FORMATTING ======
-; Fifo messages should be formatted as follows:
-; 00000000000000000000000000011111
-; --------------------------*=====
-; 26-bit message             5-bit event type (at most 32 event types can be registered in total)
-;                           single-bit flag
-.definelabel PushFormattedMessageToFifoArm9, 0x207DB20  ; r0 = event channel number (set to 0x12 for FifoHooks messages), r1 = 26-bit message, r2 = flag.
-
-.definelabel Arm9RegisterFifoCallbackHook, 0x207D284
-.definelabel RegisterIpcEventTypeCallback_Arm9, 0x207DAB0
-; Derived from the end address of the arm9 entry function zero-fill, also the load address for overlay0, which is loaded in after the callback set is complete.
-.definelabel Arm9TemporaryHook, 0x022BCA80 ; This hook will be overwritten by other code, but it will be used before that happens so it's ok.
 
 .open "arm9.bin", 0x02000000
 	.org Arm9RegisterFifoCallbackHook
@@ -68,10 +55,6 @@
 .gba
 .arm
 
-.definelabel Arm7RegisterFifoCallbackHook, 0x3801EF0 ; Address before mapping:    0x238A0D8
-.definelabel RegisterIpcEventTypeCallback_Arm7, 0x37FE39C ; Address before mapping: 0x2386584
-.definelabel DSEEventsTable, 0x20B0950
-
 .open "arm7.bin", 0x037F7E18
 	.org Arm7RegisterFifoCallbackHook
 	.area 0x4
@@ -86,7 +69,7 @@ arm7_register_callback_return:
         .word Arm7Callback
     .endarea
 
-    .org DSEEventsTable+(0xF9*4) ; Actual address: 0x20B0D34
+    .org DSEEventsTable+(0xF9*4) ; Actual address: 0x20B0D34 (us)
     .area 0x1C
     Arm7RegisterCallback:
         ; Save r0 and r1
@@ -107,7 +90,7 @@ arm7_register_callback_return:
 .close
 
 .open "arm9.bin", 0x02000000
-    .org DSEEventsTable+(0xB7*4) ; Actual address: 0x20B0C2C
+    .org DSEEventsTable+(0xB7*4) ; Actual address: 0x20B0C2C (us)
     .area 0x14
     Arm9Callback:
         ; Callback args: [event_type (7 => sound op), upperbits, flag]
@@ -116,7 +99,7 @@ arm7_register_callback_return:
         mov r3,#9 ; arm9
 
         b CallbackCommon
-    Arm7Callback: ; Actual address: 0x20B0C38
+    Arm7Callback: ; Actual address: 0x20B0C38 (us)
         ; Callback args: [event_type (7 => sound op), upperbits, flag]
         stmdb r13!,{r0-r12,r14}
 
